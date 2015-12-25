3057e5be7c6857928bf867df78bb4aeff288b712
Jon Clayden <code@clayden.org>
2015-12-25 17:15:48 +0000

    Adding hueshift function; fixes

--- R/properties.R
+++ R/properties.R
@@ -7,13 +7,18 @@
         coords(cols)[,dim]
     else
     {
-        indices <- rep(seq_along(shades), each=length(replacement))
-        cols@coords <- cols@coords[indices,]
         if (is.numeric(replacement))
+        {
+            indices <- rep(seq_along(shades), each=length(replacement))
+            cols@coords <- cols@coords[indices,,drop=FALSE]
             cols@coords[,dim] <- rep(replacement, length(shades))
+        }
         else
         {
             replacement <- match.fun(replacement)
+            temp <- replacement(cols@coords[1,dim])
+            indices <- rep(seq_along(shades), each=length(temp))
+            cols@coords <- cols@coords[indices,,drop=FALSE]
             cols@coords[,dim] <- replacement(cols@coords[,dim])
         }
         shade(cols)
@@ -32,3 +37,8 @@ brightness <- function (shades, values = NULL)
     .replaceProperty(shades, values, "HSV", 3)
 }
 
+#' @export
+hueshift <- function (shades, angles = NULL)
+{
+    .replaceProperty(shades, function(x) (x+angles) %% 360, "HSV", 1)
+}
