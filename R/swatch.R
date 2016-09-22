a27135967dc15e00bb7a0db2e9e33e7392b34a4a
Jon Clayden <code@clayden.org>
2016-09-22 17:54:37 +0100

    Adding grey border around swatch squares, for visiblility of near-white shades

--- R/swatch.R
+++ R/swatch.R
@@ -22,7 +22,7 @@ swatch <- function (x, bg = "white", ...)
     devRatio <- devSize[2] / devSize[1]
     
     plot(NA, NA, xlim=c(-0.1,1.1), ylim=0.5+c(-1,1)*devRatio*0.6, xlab="", ylab="", xaxt="n", yaxt="n", bty="n", asp=1)
-    rect(centres-width/2, 0.5-width/2, centres+width/2, 0.5+width/2, col=shades, border=NA)
+    rect(centres-width/2, 0.5-width/2, centres+width/2, 0.5+width/2, col=shades, border="grey50", lwd=2)
     
     par(oldPars)
 }
