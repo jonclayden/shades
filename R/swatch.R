c5a517cbaba64e757b52f25f4893768defc1f1cd
Jon Clayden <code@clayden.org>
2018-01-08 12:12:26 +0000

    Getting swatch() to plot multidimensional shades in a grid

--- R/swatch.R
+++ R/swatch.R
@@ -13,16 +13,25 @@
 swatch <- function (x, bg = "white", ...)
 {
     shades <- shade(x)
-    width <- 0.9 / (length(shades)+1)
-    centres <- 1:length(shades) / (length(shades)+1)
+    grid <- .dims(shades, collapse=TRUE)
+    if (length(grid) == 1)
+        grid <- c(grid, 1)
+    
+    width <- 0.9 / (max(grid) + 1)
+    gap <- 1 / (max(grid) + 1)
+    
+    # The first line generates one centre value per location in each dimension
+    # The second expands out one x and y position per shade
+    centres <- lapply(grid, function(i) gap * ((max(grid) - i) / 2 + seq_len(i)))
+    centres <- as.matrix(expand.grid(centres))
     
     oldPars <- par(mai=c(0,0,0,0), bg=bg)
+    on.exit(par(oldPars))
     
     devSize <- dev.size()
     devRatio <- devSize[2] / devSize[1]
     
+    # Centre coordinates are reversed in the y-axis so that the plot "reads" top-to-bottom
     plot(NA, NA, xlim=c(-0.1,1.1), ylim=0.5+c(-1,1)*devRatio*0.6, xlab="", ylab="", xaxt="n", yaxt="n", bty="n", asp=1)
-    rect(centres-width/2, 0.5-width/2, centres+width/2, 0.5+width/2, col=shades, border="grey50", lwd=2)
-    
-    par(oldPars)
+    rect(centres[,1]-width/2, rev(centres[,2])-width/2, centres[,1]+width/2, rev(centres[,2])+width/2, col=shades, border="grey50", lwd=2)
 }
