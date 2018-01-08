c8869bca07efb70b0368249ecc024900ad0212a5
Jon Clayden <code@clayden.org>
2018-01-08 17:23:15 +0000

    Adding test with predefined colour map

--- tests/testthat/test-10-scales.R
+++ tests/testthat/test-10-scales.R
@@ -3,4 +3,5 @@ context("Creating colour scales")
 test_that("colour scales can traverse different spaces", {
     expect_equal(gradient(c("red","blue"),3), c("#FF0000","#800080","#0000FF"), hexonly=TRUE)
     expect_equal(gradient(c("red","blue"),3,space="LAB"), c("#FF0000","#C90089","#0000FF"), hexonly=TRUE)
+    expect_equal(gradient("magma",3), c("#000004","#B63779","#FCFDBF"), hexonly=TRUE)
 })
