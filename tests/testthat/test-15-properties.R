e95aaedb71347188ce0b5b718781214f9e14dc25
Jon Clayden <code@clayden.org>
2018-04-25 10:37:14 +0100

    Adding further tests

--- tests/testthat/test-15-properties.R
+++ tests/testthat/test-15-properties.R
@@ -15,5 +15,19 @@ test_that("colour properties can be manipulated", {
     expect_equal(hue("red",delta(240)), shade("#0000FF"), hexonly=TRUE)
     expect_equal(hue("blue",delta(240)), shade("#00FF00"), hexonly=TRUE)
     expect_equal(lightness("red",scalefac(0.5)), shade("#A60000"), hexonly=TRUE)
+    expect_equal(brightness("red",scalefac(0.3,0.5)), c("#4D0000","#800000"), hexonly=TRUE)
     expect_equivalent(coords(brightness("grey40",c(0.2,0.6))), matrix(c(0,0,0,0,0.2,0.6),nrow=2))
+})
+
+test_that("manipulated colour matrices have the expected dimensions", {
+    shades <- shade(c("red","green","blue"))
+    matrix <- rep(shades, 2)
+    dim(matrix) <- c(3L,2L)
+    
+    expect_null(dim(brightness(shades,0.5)))
+    expect_equal(length(brightness(shades,0.5)), 3L)
+    expect_equal(dim(brightness(shades,c(0.4,0.6))), c(2L,3L))
+    expect_equal(brightness(shades,c(0.4,0.6)), c("#660000","#990000","#006600","#009900","#000066","#000099"), hexonly=TRUE)
+    expect_equal(dim(brightness(matrix,c(0.4,0.6))), c(2L,3L,2L))
+    expect_equal(dim(brightness(matrix,recycle(0.4,0.6))), c(3L,2L))
 })
--- tests/testthat/test-40-alpha.R
+++ tests/testthat/test-40-alpha.R
@@ -11,10 +11,15 @@ test_that("opacity can be obtained and manipulated", {
     expect_null(attr(opacity("#FF000080",delta(0.5)),"alpha"))
     
     shades <- shade(c("red","green","blue"))
+    matrix <- rep(shades, 2)
+    dim(matrix) <- c(3L,2L)
+    
     expect_null(attr(shades,"alpha"))
     expect_equal(opacity(c(shades,"#00000000")), c(1,1,1,0))
     shades[2] <- "#00FF0000"
     expect_equal(attr(shades,"alpha"), c(1,0,1))
+    expect_equal(dim(opacity(matrix,0.5)), c(3L,2L))
+    expect_equal(dim(opacity(matrix,recycle(0.4,0.6))), c(3L,2L))
     
     expect_true(shade("#FF8080") == shade("#FF8080FF"))
     expect_false(shade("#FF8080") == shade("#FF8080DD"))
