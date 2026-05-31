7c9c59691c6a9858adfe9e71494c1c9e84538aef
Jon Clayden <code@clayden.org>
2026-05-31 19:14:15 +0100

    Use bold for better readability on swatch labels, and give the user more control

--- R/swatch.R
+++ R/swatch.R
@@ -5,16 +5,20 @@
 #' one dimension then the boxes will be arranged in a grid (flattening further
 #' dimensions after the second).
 #' 
-#' @param x One or more colours, in any suitable form (see
-#'   \code{\link{shade}}).
+#' @param x One or more colours, in any suitable form (see [shade()]).
 #' @param bg A background colour.
+#' @param border The border colour to draw around each box.
+#' @param cex The font size to use for labelling named shades.
+#' @param font The font weight to use for labelling named shades, in the sense
+#'   of [par()]. The default, 2, corresponds to boldface.
 #' @param ... Additional arguments (currently unused).
 #' 
 #' @examples
 #' swatch(c("red", "green", "blue"))
+#' @seealso [par()]
 #' @author Jon Clayden <code@@clayden.org>
 #' @export
-swatch <- function (x, bg = "white", ...)
+swatch <- function (x, bg = "white", border = "grey50", cex = 0.7, font = 2, ...)
 {
     shades <- shade(x)
     grid <- .dims(shades, collapse=TRUE)
@@ -37,13 +41,13 @@ swatch <- function (x, bg = "white", ...)
     
     # Centre coordinates are reversed in the y-axis so that the plot "reads" top-to-bottom
     plot(NA, NA, xlim=c(-0.1,1.1), ylim=0.5+c(-1,1)*devRatio*0.6, xlab="", ylab="", xaxt="n", yaxt="n", bty="n", asp=1)
-    rect(centres[,1]-width/2, rev(centres[,2])-width/2, centres[,1]+width/2, rev(centres[,2])+width/2, col=shades, border="grey50", lwd=2)
+    rect(centres[,1]-width/2, rev(centres[,2])-width/2, centres[,1]+width/2, rev(centres[,2])+width/2, col=shades, border=border, lwd=2)
 
     if (.hasNames(shades))
     {
         blackContrast <- contrast(shades, "black")
         whiteContrast <- contrast(shades, "white")
         labelCol <- ifelse(blackContrast > whiteContrast, "black", "white")
-        text(centres[,1], rev(centres[,2]), names(shades), col=labelCol, cex=0.7)
+        text(centres[,1], rev(centres[,2]), names(shades), col=labelCol, cex=cex, font=font)
     }
 }
