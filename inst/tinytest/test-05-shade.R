fde0e7b01391e395351fe43c842c9f6c7c14f69c
Jon Clayden <code@clayden.org>
2026-06-29 18:02:46 +0100

    Fill main gaps in test coverage

--- inst/tinytest/test-05-shade.R
+++ inst/tinytest/test-05-shade.R
@@ -1,11 +1,22 @@
-# Create shade objects from various objects
+# Create shade objects from various objects and none
+expect_length(shade(), 0L)
 expect_equal(shade(), shade(character(0)))
+expect_equal(names(shade("red")), "red")
 expect_stdout(print(shade("red")), "1 named shade")
 expect_equal(space(shade("red")), "sRGB")
 expect_equivalent(coords("red"), matrix(c(1,0,0),nrow=1))
 expect_equivalent(shade(matrix(c(1,0,0),nrow=1)), shade("#FF0000"))
 # Using a factor to check the default initialisation method
 expect_equivalent(shade(factor(rep("red",3))), rep(shade("red"),3))
+
+shade <- shade("red")
+shade[1] <- "#0bead5"
+expect_null(names(shade))
+
+# Try the shades() interface
+randomRgbCoords <- matrix(runif(6), nrow=2)
+expect_equal(shades(red="#ee0000", random=randomRgbCoords),
+             c(shade(c(red="#ee0000")), structure(shade(randomRgbCoords), names=rep("random",2))))
 
 if (requireNamespace("colorspace", quietly=TRUE))
     expect_true(shade(colorspace::sRGB(1,0,0)) == "red")
@@ -21,6 +32,7 @@ expect_equivalent(coords(warp(warp(shade,"HSV"),"sRGB")), coords(shade), toleran
 shades <- shade(c("red","green","blue"))
 expect_true(shades[1] == "red")
 shades[1] <- "darkred"
+expect_equal(names(shades)[1], "darkred")
 expect_false(shades[1] == "red")
 
 expect_equal(c(shade("red"),shade("green")), shade(c("red","green")))
